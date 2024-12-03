79e66b5c00d1e8c9b63238476f3fde87e2826c32
psg-dsci <160556548+psg-dsci@users.noreply.github.com>
2024-12-03 14:23:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello from batch 6!'
+    return 'Hello from batch 13!'
 
 if __name__ == '__main__':
-    app.run(port=7000)
+    app.run(port=5000)
