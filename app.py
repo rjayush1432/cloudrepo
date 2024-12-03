2fc8a5e0375edf9f56dc7301f8e6df06db10cc94
psg-dsci <160556548+psg-dsci@users.noreply.github.com>
2024-12-03 11:39:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from flask import Flask; app = Flask(__name__); 
 @app.route('/') 
-def home(): return 'Hello, World!'; 
+def home(): return 'Hello from batch 6!'; 
 if __name__ == '__main__': app.run()
